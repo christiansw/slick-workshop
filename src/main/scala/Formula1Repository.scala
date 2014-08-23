d7e177ecdcfae28aaa1abf18e07d223f6f62219f
Christian Schwarz <christian.schwarz@bekk.no>
2014-08-24 00:18:02 +0200

    whitespace

--- src/main/scala/Formula1Repository.scala
+++ src/main/scala/Formula1Repository.scala
@@ -7,13 +7,9 @@ class Formula1Repository(implicit s: Session) {
   (teams.ddl ++ drivers.ddl).create
 
   def insertTeam(team: Team) = teams += team
-
   def insertDriver(driver: Driver) = drivers += driver
-
   def listTeams(implicit s: Session): List[Team] = teams.list
-
   def listDrivers(implicit s: Session): List[Driver] = drivers.list
-
   def listTeamsWithBudgetAbove(minimumBudget: Int)(implicit s: Session) = {
     val filterQuery: Query[Teams, Team, Seq] = teams.filter(_.budget > minimumBudget)
     filterQuery.list
