5f5f60e09f85f699d2293b7963a45040fa4b8568
Sindre Nordbø <sindre.nordbo@bekk.no>
2014-08-25 20:20:43 +0200

    Added test to ensure unique index on team.name

--- src/test/scala/T1_InsertAndQuery.scala
+++ src/test/scala/T1_InsertAndQuery.scala
@@ -1,4 +1,4 @@
-
+import org.h2.jdbc.JdbcSQLException
 
 class T1_InsertAndQuery extends BaseFormula1RepositoryTest {
 
@@ -9,6 +9,14 @@ class T1_InsertAndQuery extends BaseFormula1RepositoryTest {
 
     assert(results.size == 1)
     assert(results.head == Team(Some(1), "Red Bull", "Renault", 425, 710))
+  }
+
+  test("Should not allow two teams with the same name") {
+    sut.insertTeam(Team(Some(1), "Red Bull", "Renault", 425, 710))
+    val e = intercept[JdbcSQLException] {
+      sut.insertTeam(Team(Some(2), "Red Bull", "Mercedes", 600, 845))
+    }
+    assert(e.getMessage contains "Unique index or primary key violation")
   }
 
   test("Should return inserted driver") {
