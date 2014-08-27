5297943ba6ae299b782346fc0a1d91d290e5651d
Christian Schwarz <christian.schwarz@bekk.no>
2014-08-27 21:58:46 +0200

    more readable

--- src/test/scala/T4_UpdateAndDelete.scala
+++ src/test/scala/T4_UpdateAndDelete.scala
@@ -7,11 +7,11 @@ class T4_UpdateAndDelete extends BaseFormula1RepositoryTest {
 
     sut.insertTeam(Team(teamId, "Red Bull", "Renault", 425, 710))
 
-    val updatedRows = sut.update(Team(teamId, "Red Bull", "Mazda", 123, 456))
+    val updatedRows = sut.update(Team(teamId, "Red Bull", "Mazda", 111, 222))
     assert(updatedRows === 1)
 
     val updatedTeam = sut.findTeamById(teamId)
-    assert(updatedTeam === Some(Team(teamId, "Red Bull", "Mazda", 123, 456)))
+    assert(updatedTeam === Some(Team(teamId, "Red Bull", "Mazda", 111, 222)))
   }
 
   test("Should update employees on team") {
@@ -19,11 +19,11 @@ class T4_UpdateAndDelete extends BaseFormula1RepositoryTest {
 
     sut.insertTeam(Team(teamId, "Red Bull", "Renault", 425, 710))
 
-    val updatedRows = sut.updateEmployees(teamId, 800)
+    val updatedRows = sut.updateEmployees(teamId, 999)
     assert(updatedRows === 1)
 
     val updatedTeam = sut.findTeamById(teamId)
-    assert(updatedTeam === Some(Team(teamId, "Red Bull", "Renault", 425, 800)))
+    assert(updatedTeam === Some(Team(teamId, "Red Bull", "Renault", 425, 999)))
   }
 
   test("Should delete team") {
